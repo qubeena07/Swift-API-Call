8c071baaf7e00279ae389d81c76ef2932b47135c
Dipika <qubeena7@gmail.com>
2023-07-20 10:38:07 +0545

    Initial Commit

---
+++ SwiftUIAPI/SwiftUIAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPIApp.swift
+//  SwiftUIAPI
+//
+//  Created by Dipika Ranabhat on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
